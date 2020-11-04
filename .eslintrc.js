2cc68689b2ab53f0efca0ca3ce1f631ae8ee2b6f
noisyboy25 <dj402and@gmail.com>
2020-11-04 13:56:01 +0300

    Fix ESLint unix linebreak

--- .eslintrc.js
+++ .eslintrc.js
@@ -120,7 +120,7 @@ module.exports = {
         "line-comment-position": "error",
         "linebreak-style": [
             "error",
-            "windows"
+            "unix"
         ],
         "lines-around-comment": "error",
         "lines-around-directive": "error",
